3e3b155116149708e5bd7212489b6209eebd6531
Cody13X <eyannick13@gmail.com>
2015-11-27 12:33:18 +0100

    Mise à jour arrayMaker.js

--- src/ArrayMaker.js
+++ src/ArrayMaker.js
@@ -4,4 +4,12 @@ function arrayMaker(max) {
   } else if(max === 0) {
     return [];
   }
+  var tab =[];
+	var pos = 0;
+	for (var i = 1; i < max+1; i++) {
+ 		tab[pos] = i;
+    pos++;
+  }
+  return tab;
 }
+alert( arrayMaker(7) );
